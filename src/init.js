1766e0d3fcebf8c2f522a777fe991a5a91b2a1dd
Ray Krow <raykrow@Rays-MacBook-Pro.local>
2018-05-09 16:33:09 -0700

    Set init to handle directory

--- src/init.js
+++ src/init.js
@@ -1,34 +1,33 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-module.exports = function(store) {
+function initDataFromFile(store, file) {
 
   let value = null;
+  let initFileName = `./init.d/${file}`;
   let initFile = null;
 
   try {
-    initFile = fs.readFileSync('./init.d', 'utf8');
+    initFile = fs.readFileSync(initFileName, 'utf8');
   } catch (error) {
-    console.log('INIT: No init file found');
-    store.put('halo', {});
+    console.log(`INIT: No file found for ${initFileName}`);
     return;
   }
 
   try {
     value = JSON.parse(initFile);
-    console.log('INIT: Parsed init.d as json');
+    console.log(`INIT: Parsed ${initFileName} as json`);
   } catch(error) { }
 
   if (!value) {
     try {
       value = yaml.safeLoad(initFile);
-      console.log('INIT: Parsed init.d as yaml');
+      console.log(`INIT: Parsed ${initFileName} as yaml`);
     } catch (error) { }
   }
 
   if (!value) {
-    console.log("INIT: Failed to parse init.d");
-    store.put('halo', {});
+    console.log(`INIT: Failed to parse ${initFileName}`);
     return;
   }
 
@@ -38,6 +37,27 @@ module.exports = function(store) {
 
   store.put(key, value);
 
-  console.log("INIT: Seeded store with init.d data");
+  console.log(`INIT: Seeded store with data from ${initFileName}`);
+}
 
-};
+function initDataFromDir(store) {
+
+  store.put('halo', {});
+
+  let initFiles = [];
+
+  try {
+    initFiles = fs.readdirSync('./init.d');
+  } catch (error) {
+    console.log('INIT: No init.d dir found');
+    return;
+  }
+
+  initFiles.forEach(file => {
+    initDataFromFile(store, file);
+  });
+
+
+}
+
+module.exports = initDataFromDir;
